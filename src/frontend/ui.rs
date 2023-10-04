15815457566fdb1a64b34b5a241081afedad4140
Francis De Brabandere <francisdb@gmail.com>
2023-10-04 11:47:13 +0200

    wip

--- src/frontend/ui.rs
+++ src/frontend/ui.rs
@@ -1,3 +1,8 @@
+use ratatui::layout::{Constraint, Direction, Layout, Rect};
+use ratatui::prelude::Stylize;
+use ratatui::style::Modifier;
+use ratatui::text::Line;
+use ratatui::widgets::{Clear, ListItem};
 use ratatui::{
     layout::Alignment,
     style::{Color, Style},
@@ -7,6 +12,54 @@ use ratatui::{
 use crate::{frontend::app::App, frontend::tui::Frame};
 
 pub fn render(app: &mut App, f: &mut Frame) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(f.size());
+
+    // Iterate through all elements in the `items` app and append some debug text to it.
+    let items: Vec<ListItem> = app
+        .items
+        .items
+        .iter()
+        .map(|i| {
+            let mut lines = vec![Line::from(i.0)];
+            for _ in 0..i.1 {
+                lines.push(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                        .italic()
+                        .into(),
+                );
+            }
+            ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
+        })
+        .collect();
+
+    let items = ratatui::widgets::List::new(items)
+        .block(Block::default().borders(Borders::ALL).title("List"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol(">> ");
+
+    f.render_stateful_widget(items, chunks[0], &mut app.items.state);
+    f.render_widget(
+        Paragraph::new("Hello World!").block(
+            Block::default()
+                .borders(Borders::ALL)
+                .border_type(BorderType::Rounded),
+        ),
+        chunks[1],
+    );
+
+    dialog(app, f);
+}
+
+fn dialog(app: &mut App, f: &mut Frame) {
+    let dialog_rect = centered_rect(f.size(), 50, 50);
+    f.render_widget(Clear, dialog_rect);
     f.render_widget(
         Paragraph::new(format!(
             "
@@ -25,6 +78,32 @@ pub fn render(app: &mut App, f: &mut Frame) {
         )
         .style(Style::default().fg(Color::Yellow))
         .alignment(Alignment::Center),
-        f.size(),
+        dialog_rect,
     )
+}
+
+fn centered_rect(r: Rect, percent_x: u16, percent_y: u16) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_y) / 2),
+                Constraint::Percentage(percent_y),
+                Constraint::Percentage((100 - percent_y) / 2),
+            ]
+            .as_ref(),
+        )
+        .split(r);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_x) / 2),
+                Constraint::Percentage(percent_x),
+                Constraint::Percentage((100 - percent_x) / 2),
+            ]
+            .as_ref(),
+        )
+        .split(popup_layout[1])[1]
 }
