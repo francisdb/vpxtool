f7778aba6c5e8db1390c7eb6a48f78222e55b942
Francis De Brabandere <francisdb@gmail.com>
2023-10-04 11:47:13 +0200

    wip

--- src/frontend/app.rs
+++ src/frontend/app.rs
@@ -1,13 +1,65 @@
+use ratatui::widgets::ListState;
+
 /// Application.
+
 #[derive(Debug, Default)]
-pub struct App {
+pub struct StatefulList<T> {
+    pub state: ListState,
+    pub items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct App<'a> {
     /// should the application exit?
     pub should_quit: bool,
     /// counter
     pub counter: u8,
+
+    pub items: StatefulList<(&'a str, usize)>,
 }
 
-impl App {
+impl<'a> App<'a> {
     /// Constructs a new instance of [`App`].
     pub fn new() -> Self {
         Self::default()
